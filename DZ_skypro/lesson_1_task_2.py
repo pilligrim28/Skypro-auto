f99a0df36a8af90a9e3246c26ba7ffd8fca5f4c8
Александр Николаев <alexandrnikolaev1@gmail.com>
2023-01-25 21:39:03 +0300

    skypro

---
+++ DZ_skypro/lesson_1_task_2.py
@@ -0,0 +1,2 @@
+name ="Саша"
+print(name)
---
+++ lesson1.py
@@ -0,0 +1,15 @@
+# weekDays = [
+#    "Понедельник",
+#     "Вторник",
+#     "Среда",
+#     "Четверг",
+#     "Пятница",
+#     "Суббота",
+#     "Воскресенье"
+
+# ]
+# lenght = len(weekDays)
+# print(lenght)
+# monday = weekDays[0]
+# print(monday)
+print("Привет \n" *  11)
