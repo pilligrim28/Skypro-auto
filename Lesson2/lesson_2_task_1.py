2c334c577b85741629011d55652a839b2580b641
Александр Николаев <alexandrnikolaev1@gmail.com>
2023-01-31 20:09:32 +0300

    2

---
+++ Lesson2/lesson_2_task_1.py
@@ -0,0 +1,4 @@
+lst = ( '🍇', '🍑', '🍐', '🍊', '🍌', '🍎')
+for a in range (0,5):
+    print(lst[0])
+    print(lst[5])
---
+++ Lesson2/lesson_2_task_2.py
@@ -0,0 +1,12 @@
+def is_year_leap(year):
+    year = input(int("ВВедите год:"))
+   
+print(year)
+
+if (year % 4 == 0):
+
+print('True')
+
+else:
+
+print('False')
